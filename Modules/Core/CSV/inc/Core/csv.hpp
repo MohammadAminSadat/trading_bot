0471272fc0953cb65cea60e5060d6da286bbe5dd
Mohammad Amin Sadat <s.mohammad.amin.sadat@gmail.com>
2026-08-05 20:08:07 +0330

    add csv parsing function

--- Modules/Core/CSV/inc/Core/csv.hpp
+++ Modules/Core/CSV/inc/Core/csv.hpp
@@ -6,27 +6,31 @@
 #include <vector>
 
 namespace TradingEngine::Core {
-
+using CSVRow = std::vector<std::string>;
 class CSV {
 public:
   explicit CSV(std::filesystem::path path, std::string delimiter = ",", bool has_header = false);
+  virtual ~CSV();
   CSV(CSV &&other) noexcept;
   CSV &operator=(CSV &&other) noexcept;
 
   CSV(const CSV &other) = delete;
   CSV &operator=(const CSV &other) = delete;
 
-  std::vector<std::string> read_header();
-  std::vector<std::string> read_next();
+  CSVRow read_header();
+  CSVRow read_next();
+  std::vector<CSVRow> parse_csv();
 
 private:
-  std::vector<std::string> parse_line(const std::string &line);
+  CSVRow parse_line(const std::string &line);
 
   bool has_header{false};
   bool is_header_processed{false};
+  bool is_cached{false};
+  std::vector<CSVRow> cache;
   std::filesystem::path path;
   std::string delimiter;
-  std::ifstream file;
+  std::ifstream csv_file;
 };
 
 } // namespace TradingEngine::Core
--- Modules/Core/CSV/src/csv.cpp
+++ Modules/Core/CSV/src/csv.cpp
@@ -9,12 +9,12 @@ CSV::CSV(std::filesystem::path path, std::string delimiter, bool has_header)
     : has_header{has_header}, delimiter{std::move(delimiter)} {
   if (!std::filesystem::exists(path)) {
     std::stringstream error;
-    error << "file: " << path << " does not exist";
+    error << "csv_file: " << path << " does not exist";
     throw std::runtime_error(error.str());
   }
   is_header_processed = !has_header;
-  file.open(path);
-  if (!file.is_open()) {
+  csv_file.open(path);
+  if (!csv_file.is_open()) {
     std::stringstream error;
     error << "file: " << path << " could not be opened";
     throw std::runtime_error(error.str());
@@ -22,12 +22,17 @@ CSV::CSV(std::filesystem::path path, std::string delimiter, bool has_header)
   this->path = std::move(path);
 }
 
+CSV::~CSV() {
+  if (csv_file.is_open()) {
+    csv_file.close();
+  }
+}
 CSV::CSV(CSV &&other) noexcept
     : has_header{other.has_header},
       is_header_processed{other.is_header_processed},
       path{std::move(other.path)},
       delimiter{std::move(other.delimiter)},
-      file{std::move(other.file)} {
+      csv_file{std::move(other.csv_file)} {
   other.has_header = false;
   other.is_header_processed = false;
 }
@@ -38,14 +43,14 @@ CSV &CSV::operator=(CSV &&other) noexcept {
     is_header_processed = other.is_header_processed;
     path = std::move(other.path);
     delimiter = std::move(other.delimiter);
-    file = std::move(other.file);
+    csv_file = std::move(other.csv_file);
     other.has_header = false;
     other.is_header_processed = false;
   }
   return *this;
 }
 
-std::vector<std::string> CSV::read_header() {
+CSVRow CSV::read_header() {
   if (!has_header) {
     return {};
   }
@@ -53,23 +58,41 @@ std::vector<std::string> CSV::read_header() {
     return {};
   }
   std::string line;
-  if (!std::getline(file, line)) {
+  if (!std::getline(csv_file, line)) {
     return {};
   }
   is_header_processed = true;
   return parse_line(line);
 }
 
-std::vector<std::string> CSV::read_next() {
+CSVRow CSV::read_next() {
   std::string line;
-  if (!std::getline(file, line)) {
+  if (!std::getline(csv_file, line)) {
     return {};
   }
   return parse_line(line);
 }
 
-std::vector<std::string> CSV::parse_line(const std::string &line) {
-  std::vector<std::string> fields;
+std::vector<CSVRow> CSV::parse_csv() {
+  std::vector<CSVRow> data;
+  if (!is_header_processed) {
+    read_header();
+  }
+  if (is_cached) {
+    return cache;
+  }
+  std::string line;
+  while (std::getline(csv_file, line)) {
+    CSVRow row{parse_line(line)};
+    data.emplace_back(row);
+  }
+  is_cached = true;
+  cache = data;
+  return data;
+}
+
+CSVRow CSV::parse_line(const std::string &line) {
+  CSVRow fields;
   size_t position{0};
 
   while (position <= line.length()) {
