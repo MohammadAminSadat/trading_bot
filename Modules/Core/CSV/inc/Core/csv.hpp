068861310212101e837475360572323fefca0a83
Mohammad Amin Sadat <amin.sadat@sw-motion.cn>
2026-08-06 12:01:11 +0330

    add CSVReaderIterator to iterate CSV rows

--- Modules/Core/CSV/inc/Core/csv.hpp
+++ Modules/Core/CSV/inc/Core/csv.hpp
@@ -1,13 +1,58 @@
 #pragma once
 
+#include <cstddef>
 #include <filesystem>
 #include <fstream>
+#include <iterator>
 #include <optional>
 #include <string>
+#include <string_view>
 #include <vector>
 
 namespace TradingEngine::Core {
 using CSVRow = std::vector<std::string>;
+class CSVReader;
+
+class CSVReaderIterator {
+public:
+  using iterator_category = std::input_iterator_tag;
+  using value_type = CSVRow;
+  using difference_type = std::ptrdiff_t;
+  using pointer = CSVRow *;
+  using reference = CSVRow &;
+
+  CSVReaderIterator() = default;
+  explicit CSVReaderIterator(CSVReader &reader);
+
+  CSVRow &operator*() noexcept { return *current; };
+  CSVRow *operator->() noexcept { return &*current; };
+  CSVRow const &operator*() const noexcept { return *current; };
+  CSVRow const *operator->() const noexcept { return &*current; };
+
+  CSVReaderIterator &operator++();
+  CSVReaderIterator operator++(int);
+
+  bool operator==(const CSVReaderIterator &other) const noexcept {
+    return (!current.has_value() && !other.current.has_value());
+  }
+  bool operator!=(const CSVReaderIterator &other) const noexcept { return !(*this == other); }
+
+  bool operator==(std::default_sentinel_t) const noexcept {
+    return (reader == nullptr || !current.has_value());
+  }
+  bool operator!=(std::default_sentinel_t s) const noexcept { return !(*this == s); };
+
+private:
+  CSVReader *reader{nullptr};
+  std::optional<CSVRow> current{std::nullopt};
+};
+
+inline bool operator==(std::default_sentinel_t s, const CSVReaderIterator &it) noexcept {
+  return it == s;
+}
+inline bool operator!=(std::default_sentinel_t s, const CSVReaderIterator &it) noexcept {
+  return it != s;
+}
 
 class CSVLineParser {
 public:
@@ -21,6 +66,9 @@ public:
   ~CSVReader() = default;
   CSVReader(CSVReader &&other) = default;
   CSVReader &operator=(CSVReader &&other) = default;
+  bool operator!() const noexcept { return !csv_file; }
+  auto begin() { return CSVReaderIterator(*this); };
+  auto end() { return CSVReaderIterator{}; }
 
   CSVReader(const CSVReader &other) = delete;
   CSVReader &operator=(const CSVReader &other) = delete;
@@ -48,5 +96,20 @@ private:
   std::string delimiter;
   CSVRow header;
 };
+
+inline CSVReaderIterator ::CSVReaderIterator(CSVReader &reader) : reader{&reader} {
+  current = reader.get_next();
+}
+
+inline CSVReaderIterator &CSVReaderIterator ::operator++() {
+  current = reader->get_next();
+  return *this;
+}
+
+inline CSVReaderIterator CSVReaderIterator ::operator++(int) {
+  auto temp{*this};
+  current = reader->get_next();
+  return temp;
+}
 
 } // namespace TradingEngine::Core
