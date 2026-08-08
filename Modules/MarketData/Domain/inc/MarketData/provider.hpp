d3112d530d0a5a60929066deb5225a68e9c65235
Mohammad Amin Sadat <s.mohammad.amin.sadat@gmail.com>
2026-08-08 18:27:04 +0330

    refactor(MarketData) : rename Core to Domain

--- Modules/MarketData/Domain/inc/MarketData/provider.hpp
+++ Modules/MarketData/Domain/inc/MarketData/provider.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <concepts>
 #include <optional>
+#include <ranges>
 
 #include "MarketData/candle.hpp"
 
@@ -10,5 +11,9 @@ template <typename T>
 concept HistoricalProvider = requires(T& provider) {
   { provider.get_next() } -> std::same_as<std::optional<Candle>>;
 };
+
+template <typename T>
+concept HistoricalRange =
+    std::ranges::input_range<T> && std::same_as<std::ranges::range_value_t<T>, Candle>;
 
 } // namespace TradingEngine::MarketData
--- Modules/MarketData/Providers/CSVProvider/inc/MarketData/CSVProvider.hpp
+++ Modules/MarketData/Providers/CSVProvider/inc/MarketData/CSVProvider.hpp
@@ -5,6 +5,7 @@
 #include <cstdint>
 #include <filesystem>
 #include <functional>
+#include <iterator>
 #include <sstream>
 #include <string>
 #include <string_view>
@@ -12,7 +13,7 @@
 namespace TradingEngine::MarketData {
 
 using TimestampParser = std::function<Core::Timestamp(std::string_view)>;
-Core::Timestamp default_timestamp_parser(std::string_view);
+inline Core::Timestamp default_timestamp_parser(std::string_view);
 struct ColumnMapping {
   size_t timestamp;
   size_t open;
@@ -39,13 +40,69 @@ struct CSVImporterConfiguration {
 
 class CSVProvider {
 public:
+  class Iterator {
+  public:
+    using iterator_category = std::input_iterator_tag;
+    using value_type = Candle;
+    using difference_type = std::ptrdiff_t;
+    using pointer = const Candle *;
+    using reference = const Candle &;
+
+    Iterator() = default;
+    explicit Iterator(CSVProvider &provider) : provider{&provider}, current{provider.get_next()} {};
+
+    reference operator*() const noexcept { return *current; };
+    pointer operator->() const noexcept { return &*current; };
+
+    Iterator &operator++() {
+      if (!provider) {
+        throw std::logic_error("Incrementing end iterator");
+      }
+      current = provider->get_next();
+      return *this;
+    };
+    Iterator operator++(int) {
+      if (!provider) {
+        throw std::logic_error("Incrementing end iterator");
+      }
+      auto temp{*this};
+      current = provider->get_next();
+      return temp;
+    };
+
+    bool operator==(std::default_sentinel_t) const noexcept {
+      return (provider == nullptr || !current.has_value());
+    }
+    bool operator!=(std::default_sentinel_t s) const noexcept { return !(*this == s); };
+
+  private:
+    std::optional<Candle> current;
+    CSVProvider *provider{nullptr};
+  };
+
+  using iterator = Iterator;
+  using sentinel = std::default_sentinel_t;
+
   CSVProvider(CSVImporterConfiguration configs);
   ~CSVProvider() = default;
   std::optional<Candle> get_next();
+  void reset() { reader.reset(); };
+  iterator begin() {
+    reset();
+    return Iterator(*this);
+  };
+  sentinel end() noexcept { return std::default_sentinel; }
 
 private:
   CSVImporterConfiguration configs;
   Core::CSVReader reader;
 };
+
+inline bool operator==(std::default_sentinel_t s, const CSVProvider::Iterator &it) noexcept {
+  return it == s;
+}
+inline bool operator!=(std::default_sentinel_t s, const CSVProvider::Iterator &it) noexcept {
+  return it != s;
+}
 
 } // namespace TradingEngine::MarketData
