7990924469769812b4e4d9acbca6ec7c68bbbd0f
Mohammad Amin Sadat <amin.sadat@sw-motion.cn>
2026-08-09 13:52:09 +0330

    feature(Indicator): add IIndicator abstraction and make the Domain INTERFACE library

--- Modules/Indicators/Domain/inc/Indicators/indicators.hpp
+++ Modules/Indicators/Domain/inc/Indicators/indicators.hpp
@@ -1,1 +1,39 @@
 #pragma once
+#include <Core/Core.hpp>
+#include <MarketData/candle.hpp>
+#include <optional>
+#include <vector>
+
+namespace TradingEngine::Indicators {
+
+struct IndicatorOutput {
+  Core::Timestamp timestamp;
+  double signal1;
+  double signal2;
+};
+
+enum class InputData {
+  Open,
+  High,
+  Low,
+  Close,
+  Volume,
+};
+
+enum class SignalType {
+  ZeroCross,
+  TwoLineCross,
+  PriceCross,
+};
+
+using InputRequirements = std::vector<InputData>;
+
+class IIndicator {
+public:
+  virtual ~IIndicator() = default;
+  virtual std::optional<IndicatorOutput> update(const MarketData::Candle &) = 0;
+  [[nodiscard]] virtual const InputRequirements &requirements() const = 0;
+  [[nodiscard]] virtual SignalType signal_type() const noexcept = 0;
+};
+
+} // namespace TradingEngine::Indicators
