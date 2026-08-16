0663b57eb5694e68efc8ddbbece9e24f9e740f43
Mohammad Amin Sadat <amin.sadat@sw-motion.cn>
2026-08-16 17:16:23 +0330

    feature(CommandParser): add basic types for parser

--- CLI/Domain/inc/CLI/Command.hpp
+++ CLI/Domain/inc/CLI/Command.hpp
@@ -1,20 +1,20 @@
 #pragma once
 #include <string_view>
+#include <vector>
 
 namespace TradingEngine::CLI {
 enum class CommandType {
-  UTIL,
-  DATA,
-  INDICATOR,
-  STRATEGY,
-  RISK,
-  SUMMARY,
-  LOGS,
-  OTHERS,
+  UTILITY_COMMAND,
+  DATA_COMMAND,
+  INDICATOR_COMMAND,
+  STRATEGY_COMMAND,
+  RISK_COMMAND,
+  BACKTEST_COMMAND,
 };
 
 struct Command {
   CommandType type;
-  std::string_view args;
+  std::string_view sub_command;
+  std::vector<std::string_view> arguments;
 };
 } // namespace TradingEngine::CLI
